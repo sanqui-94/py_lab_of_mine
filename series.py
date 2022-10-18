ee6ab6515a3c71a0fcdb9874029d03de5a206746
Santiago Quiroga Turdera <santiago.quiroga@truextend.com>
2022-10-18 18:03:12 -0400

    using with block

--- series.py
+++ series.py
@@ -4,11 +4,8 @@ import sys
 
 
 def read_series(filename):
-    try:
-        f = open(filename, mode='rt', encoding='utf-8')
-        return [ int(line.strip()) for line in f ]
-    finally:
-        f.close()
+    with open(filename, mode='rt', encoding='utf-8') as f:
+        return [int(line.strip()) for line in f]
 
 
 def main(filename):
