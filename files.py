ace201fa33a8ad9f5e44f6e6d7fef3e8122d27b6
Santiago Quiroga Turdera <santiago.quiroga@truextend.com>
2022-10-18 17:57:48 -0400

    exercises with open

---
+++ files.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def main(filename):
+    f = open(filename, mode='rt', encoding='utf-8')
+    for line in f:
+        print(line)
+    f.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
